04a86961c1af5670957397c5df17676c728678ec
Sergio Losada <sergio.losada@outlook.com>
2023-11-14 00:26:44 +0100

    PHPMailer

--- php/formulario.php
+++ php/formulario.php
@@ -1,9 +1,17 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $nombre = $_POST["nombre"];
-  $email = $_POST["email"];
-  $asunto = $_POST["asunto"];
-  $mensaje = $_POST["mensaje"];
+  $nombre = htmlspecialchars($_POST["nombre"]);
+  $email = htmlspecialchars($_POST["email"]);
+  $asunto = htmlspecialchars($_POST["asunto"]);
+  $mensaje = htmlspecialchars($_POST["mensaje"]);
 
   // Verifica que los campos no estén vacíos
   if (empty($nombre) || empty($email) || empty($asunto) || empty($mensaje)) {
@@ -19,11 +27,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mensajeCorreo .= "Mensaje:\n$mensaje";
 
     // Envía el correo electrónico
-    if (mail($destinatario, "Mensaje de contacto", $mensajeCorreo)) {
-      echo "Mensaje enviado con éxito. Gracias por contactarnos.";
-    } else {
-      echo "Hubo un problema al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.";
-    }
+    // if (mail($destinatario, "Mensaje de contacto", $mensajeCorreo)) {
+    //   echo "Mensaje enviado con éxito. Gracias por contactarnos.";
+    // } else {
+    //   echo "Hubo un problema al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.";
+    // }
+    $mail = new PHPMailer(true);
+    // $mail->isSMTP();
+    $mail->setFrom($email);
+    $mail->addAddress('emilioarenasmayor@gmail.com');
+    $mail->Subject = $asunto;
+    $mail->Body = $mensaje;
+
+    $mail->send();
   }
 }
 ?>
